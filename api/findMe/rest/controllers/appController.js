6dbdff1a75008534af215f1e1178585d7887a4e2
Luuk Cloosterman <461105@student.saxion.nl>
2020-01-25 17:12:44 +0100

    user stuffies done

---
+++ api/findMe/rest/controllers/appController.js
@@ -0,0 +1,39 @@
+module.exports = (app) => {
+    app.get('/', (req, res, next) => {
+        res.status(200).json("It works mothafockas")
+    });
+
+    app.get('/id', (req, res, next) => {
+
+        let postquery = 'CALL getuid();';
+        const values = null;
+        let uid;
+        app.db.query(postquery, function (err, result) {
+            if (err) throw err;
+            uid = result[0][0].uid;
+            let postQuery = 'INSERT INTO uloc (uid, longitude, latitude) VALUES(?, ?, ?);'
+            let params = [
+                uid,
+                req.body.longi,
+                req.body.lat
+            ];
+            app.db.query(postQuery, params, function (err, result) {
+                if (err) throw err;
+            });
+            res.status(200).json(uid);
+        });
+    });
+
+    app.post('/', (req, res, next )=> {
+        let postQuery = 'INSERT INTO uloc (uid, long, lat) VALUES(?, ?, ?)'
+        let params = [
+            req.body.id,
+            req.body.long,
+            req.body.lat
+        ];
+        app.db.query(postQuery, params, function (err, result) {
+            if (err) throw err;
+            res.status(201).json("Added succesfully")
+        } )
+    });
+};
--- api/findMe/rest/server.js
+++ api/findMe/rest/server.js
@@ -1,7 +1,7 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 
-var app         = express(); // Please do not remove this line, since CLI uses this line as guidance to import new controllers
+let app         = express(); // Please do not remove this line, since CLI uses this line as guidance to import new controllers
 let db          = require('./db/db');
 app.db          = db;
 app.db.connect(function(err){
@@ -13,7 +13,8 @@ app.db.connect(function(err){
 });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+require('./controllers/appController')(app);
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running');
 });
+
