e77447bcc88f40532498704f65259c4db2186626
MLindt1807 <47355174+MLindt1807@users.noreply.github.com>
2020-01-25 10:21:05 +0100

    Create ddl.sql

---
+++ sql/ddl.sql
@@ -0,0 +1,35 @@
+START TRANSACTION;
+CREATE DATABASE  findMe;
+
+USE findMe;
+
+CREATE TABLE user(
+	uid INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (uid)
+);
+
+CREATE TABLE uLoc(
+	uid INT NOT NULL,
+	longitude DECIMAL(9.6) NOT NULL,
+	latitude DECIMAL(9.6) NOT NULL,
+	PRIMARY KEY(uid)
+);
+
+CREATE TABLE gemLoc(
+	uid	INT NOT NULL,
+    longitude DECIMAL(9.6) NOT NULL,
+    latitude DECIMAL(9.6) NOT NULL,
+    PRIMARY KEY(longitude, latitude)
+);
+
+ALTER TABLE uLoc
+ADD CONSTRAINT FK_uLoc_ref_user
+FOREIGN KEY (uid) REFERENCES uLoc(uid);
+
+ALTER TABLE gemLoc
+ADD CONSTRAINT FK_gemLoc_ref_user
+FOREIGN KEY (uid) REFERENCES gemLoc(uid);
+    
+
+commit;
