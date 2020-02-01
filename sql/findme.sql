a963a3b4e26a052927fa8c23992f2b40affdacf4
MLindt1807 <47355174+MLindt1807@users.noreply.github.com>
2020-02-01 14:12:37 +0100

    calcdist + setmeeting

--- sql/findme.sql
+++ sql/findme.sql
@@ -50,3 +50,39 @@ VALUES();
 select LAST_INSERT_ID() AS uid; 
 END ;;
 DELIMITER ;
+
+
+
+DROP PROCEDURE IF EXISTS `calcDist`;
+DELIMITER ;;
+CREATE PROCEDURE `calcDist`(lat1 decimal(9,6), lon1 decimal(9,6), lat2 decimal(9,6), lon2 decimal(9,6),out dist decimal(9,6))
+BEGIN 
+	
+
+set @a = 0.5 - cos((lat2 - lat1) * (pi() / 180))/2 + 
+          cos(lat1 * (pi() / 180)) * cos(lat2 * (pi() / 180)) * 
+          (1 - cos((lon2 - lon1) * (pi() / 180)))/2;
+
+set dist = 12742 * asin(sqrt(@a));
+
+END ;;
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS `setMeeting`;
+DELIMITER ;;
+CREATE PROCEDURE `setMeeting`(UID1 int,lat1 decimal(9,6), lon1 decimal(9,6), UID2 int, lat2 decimal(9,6), lon2 decimal(9,6))
+BEGIN 
+
+set @lat = (lat1 + lat2) / 2;
+set @lon = (lon1 + lon2) / 2;
+
+
+insert into gemLoc(uid,latitude,longitude)
+values (uid1,ROUND(@lat,9),ROUND(@lon,9)),
+	   (uid2,ROUND(@lat,9),ROUND(@lon,9));
+
+
+
+END ;;
+DELIMITER ;
